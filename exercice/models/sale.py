345e154446dee45424a1ae5cabfe88e775b6de84
Fabien Brault <fabienbrault88@gmail.com>
2018-12-13 11:37:30 +0100

    Implémentation terminée

--- exercice/models/sale.py
+++ exercice/models/sale.py
@@ -6,6 +6,4 @@ class SaleOrder(models.Model):
 
     _inherit = "sale.order"
 
-    # Ajouter un champ texte nommé « Exercice »
-    # Ajouter une traduction en anglais et français
-    # Tout ajout supplémentaire (champ calculé, relation…) qui démontrera une recherche approfondie sera apprécié
+    exercice = fields.Text(string='Exercice', translate=True)
