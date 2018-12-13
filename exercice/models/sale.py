d048dc2a4213eb0df4c91a5cc97ef39412dec969
Fabien Brault <fabienbrault88@gmail.com>
2018-12-13 12:48:45 +0100

    Optional code

--- exercice/models/sale.py
+++ exercice/models/sale.py
@@ -6,4 +6,13 @@ class SaleOrder(models.Model):
 
     _inherit = "sale.order"
 
+    @api.one
+    @api.depends("exercice")
+    def get_compute_exercice(self) :
+    	self.compute_exercice = "compute " + self.exercice
+
     exercice = fields.Text(string='Exercice', translate=True)
+
+    compute_exercice = fields.Text(string="Compute Exercice", compute="get_compute_exercice")
+
+    relation_exercice = fields.Many2one('res.partner', string="Relation Exercice")
